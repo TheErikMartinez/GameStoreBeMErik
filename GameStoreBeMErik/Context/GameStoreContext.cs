b5975d005ab68663dcf099e13892f885701a1126
HUNGARIA\MARTINEZ <erik.adan.martinez@koerber.com>
2023-10-20 15:14:24 +0200

    update 2

--- GameStoreBeMErik/Context/GameStoreContext.cs
+++ GameStoreBeMErik/Context/GameStoreContext.cs
@@ -1,6 +1,17 @@
-﻿namespace GameStoreBeMErik.Context
+﻿using GameStoreBeMErik.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace GameStoreBeMErik.Context
 {
-    public class GameStoreContext
+    public class GameStoreContext : DbContext
     {
+        public DbSet<User> Users { get; set; }
+        public DbSet<VideoGame> VideoGames { get; set; }
+
+        //protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        //{
+        //    optionsBuilder.UseSqlServer(
+        //        @"Server=(localdb)\mssqllocaldb;Database=GameStore;Trusted_Connection=True");
+        //}
     }
 }
